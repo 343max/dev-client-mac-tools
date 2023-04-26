dd9c9d5faad6c1c225ffa87d1845aae3f2406041
Max von Webel <max@343max.de>
2023-04-26 11:46:25 +0200

    Added fallback for Android

---
+++ android/src/main/java/de/max343/MacDevTools/MacDevToolsModule.kt
@@ -0,0 +1,21 @@
+package de.max343.MacDevTools
+
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+
+class MacDevToolsModule : Module() {
+  // Each module class must implement the definition function. The definition consists of components
+  // that describes the module's functionality and behavior.
+  // See https://docs.expo.dev/modules/module-api for more details about available components.
+  override fun definition() = ModuleDefinition {
+    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
+    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
+    // The module will be accessible from `requireNativeModule('DevClientMacTools')` in JavaScript.
+    Name("MacDevTools")
+
+    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
+    Function("enabled") {
+      false
+    }
+  }
+}
