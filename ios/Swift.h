2ce11f3b66ee3cadfad5b73a9f04fd977de277bb
Max von Webel <max@343max.de>
2023-07-12 00:17:47 +0200

    fix

--- ios/Swift.h
+++ ios/Swift.h
@@ -1,4 +1,4 @@
-// When `use_frameworks!` is used, the generated Swift header is inside ExpoModulesCore module.
+// When `use_frameworks!` is used, the generated Swift header is inside MacDevTools module.
 // Otherwise, it's available only locally with double-quoted imports.
 #if __has_include(<MacDevTools/MacDevTools-Swift.h>)
 #import <MacDevTools/MacDevTools-Swift.h>
